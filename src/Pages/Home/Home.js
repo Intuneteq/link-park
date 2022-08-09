9cb16559a809b508e470016b1bf95c7a7d1f2d75
Intuneteq <97626624+Intuneteq@users.noreply.github.com>
2022-08-09 05:20:36 +0100

    Deploy Test complete for background image

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,25 +1,24 @@
 import React from "react";
 
-// import Experience from "../../Components/Experience/Experience";
-// import Header from "../../Components/Header/Header";
-// import School from "../../Components/School/School";
-// import Navbar from "../../Components/Navbar/Navbar";
-// import Statistics from "../../Components/Statistics/Statistics";
-// import Parent from "../../Components/Parent/Parent";
-// import Footer from "../../Components/Footer/Footer";
+import Experience from "../../Components/Experience/Experience";
+import Header from "../../Components/Header/Header";
+import School from "../../Components/School/School";
+import Navbar from "../../Components/Navbar/Navbar";
+import Statistics from "../../Components/Statistics/Statistics";
+import Parent from "../../Components/Parent/Parent";
+import Footer from "../../Components/Footer/Footer";
 import "./Home.scss";
 
 const Home = () => {
   return (
     <div className="home">
-      ok
-      {/* <Navbar />
+      <Navbar />
       <Header />
       <Experience />
       <School />
       <Statistics />
       <Parent />
-      <Footer /> */}
+      <Footer />
     </div>
   );
 };
